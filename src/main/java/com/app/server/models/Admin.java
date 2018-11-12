c7d70ddf400d2a4de8bff2929822694491693c39
Yizhong Chen <joshchenchina@gmail.com>
2018-11-11 22:14:22 -0800

    fixing bugs, admin hashing

--- src/main/java/com/app/server/models/Admin.java
+++ src/main/java/com/app/server/models/Admin.java
@@ -1,5 +1,10 @@
 package com.app.server.models;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
 public class Admin {
     String adminID = null;
     String username;
@@ -11,7 +16,7 @@ public class Admin {
 
     public Admin(String username, String password) {
         this.username = username;
-        this.password = password;
+        this.password = setPassword(password);
     }
 
     public String getAdminID() {
@@ -34,7 +39,53 @@ public class Admin {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String setPassword(String password) {
+        try {
+            byte[] salt = getSalt();
+            password = getSecurePassword(password, salt);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return password;
+    }
+
+    private static String getSecurePassword(String passwordToHash, byte[] salt)
+    {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(salt);
+            //Get the hash's bytes
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    //Add salt
+    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt;
     }
 }
